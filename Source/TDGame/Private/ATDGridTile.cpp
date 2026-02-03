6633f648caa282635a6c32ccd59c2359a91c899f
Parth <vaderaparth22@gmail.com>
2026-02-02 19:39:25 -0500

    Add ATDGridTile actor, blueprints, and path material

---
+++ Source/TDGame/Private/ATDGridTile.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ATDGridTile.h"
+
+// Sets default values
+AATDGridTile::AATDGridTile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AATDGridTile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AATDGridTile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
