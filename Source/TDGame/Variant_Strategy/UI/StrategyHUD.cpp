90881f4e54b63d33e0395ed4c24e2d9944f82d8a
Parth <vaderaparth22@gmail.com>
2026-03-03 21:44:32 -0500

    Add health bar + remove unused UI + more spline improvements

--- Source/TDGame/Variant_Strategy/UI/StrategyHUD.cpp
+++ Source/TDGame/Variant_Strategy/UI/StrategyHUD.cpp
@@ -11,20 +11,20 @@ void AStrategyHUD::BeginPlay()
 	Super::BeginPlay();
 
 	// spawn the UI widget
-	UIWidget = CreateWidget<UStrategyUI>(GetOwningPlayerController(), UIWidgetClass);
-	check(UIWidget);
+	//UIWidget = CreateWidget<UStrategyUI>(GetOwningPlayerController(), UIWidgetClass);
+	//check(UIWidget);
 
-	// add the UI widget to the screen
-	UIWidget->AddToViewport(0);
+	//// add the UI widget to the screen
+	//UIWidget->AddToViewport(0);
 }
 
 void AStrategyHUD::DragSelectUpdate(FVector2D Start, FVector2D WidthAndHeight, FVector2D CurrentPosition, bool bDraw)
 {
 	// copy the selection box data
-	bDrawBox = bDraw;
+	/*bDrawBox = bDraw;
 	BoxStart = Start;
 	BoxSize = WidthAndHeight;
-	BoxCurrentPosition = CurrentPosition;
+	BoxCurrentPosition = CurrentPosition;*/
 
 }
 
@@ -34,44 +34,44 @@ void AStrategyHUD::DrawHUD()
 	Super::DrawHUD();
 
 	// ensure we have a valid player controller
-	if (AStrategyPlayerController* PC = Cast<AStrategyPlayerController>(GetOwningPlayerController()))
-	{
-		// draw the selection box
-		if (bDrawBox)
-		{
-			DrawRect(SelectionBoxColor, BoxStart.X, BoxStart.Y, BoxSize.X, BoxSize.Y);
+	//if (AStrategyPlayerController* PC = Cast<AStrategyPlayerController>(GetOwningPlayerController()))
+	//{
+	//	// draw the selection box
+	//	if (bDrawBox)
+	//	{
+	//		DrawRect(SelectionBoxColor, BoxStart.X, BoxStart.Y, BoxSize.X, BoxSize.Y);
 
-			// get all the units in the selection box
-			TArray<AStrategyUnit*> BoxedUnits;
-			GetActorsInSelectionRectangle(BoxStart, BoxCurrentPosition, BoxedUnits, true);
+	//		// get all the units in the selection box
+	//		TArray<AStrategyUnit*> BoxedUnits;
+	//		GetActorsInSelectionRectangle(BoxStart, BoxCurrentPosition, BoxedUnits, true);
 
-			// update the unit selection on the player controller
-			PC->DragSelectUnits(BoxedUnits);
-		}
+	//		// update the unit selection on the player controller
+	//		PC->DragSelectUnits(BoxedUnits);
+	//	}
 
-		// get the currently selected units
-		TArray<AStrategyUnit*> SelectedUnits = PC->GetSelectedUnits();
+	//	// get the currently selected units
+	//	TArray<AStrategyUnit*> SelectedUnits = PC->GetSelectedUnits();
 
-		// update the selection count on the UI widget
-		UIWidget->SetSelectedUnitsCount(SelectedUnits.Num());
+	//	// update the selection count on the UI widget
+	//	UIWidget->SetSelectedUnitsCount(SelectedUnits.Num());
 
-		// process each selected unit
-		for (AStrategyUnit* CurrentUnit : SelectedUnits)
-		{
-			if (IsValid(CurrentUnit))
-			{
-				// project the unit's location to screen coordinates
-				FVector2D ScreenCoords;
+	//	// process each selected unit
+	//	for (AStrategyUnit* CurrentUnit : SelectedUnits)
+	//	{
+	//		if (IsValid(CurrentUnit))
+	//		{
+	//			// project the unit's location to screen coordinates
+	//			FVector2D ScreenCoords;
 
-				if (PC->ProjectWorldLocationToScreen(CurrentUnit->GetActorLocation(), ScreenCoords, true))
-				{
-					// draw a selection string near the unit
-					const FString SelectionString = "Selected";
-					DrawText(SelectionString, FColor::White, ScreenCoords.X - 25.0f, ScreenCoords.Y + 25.0f, nullptr, 1.5f);
-				}
-			}
-			
-		}
-	}
+	//			if (PC->ProjectWorldLocationToScreen(CurrentUnit->GetActorLocation(), ScreenCoords, true))
+	//			{
+	//				// draw a selection string near the unit
+	//				const FString SelectionString = "Selected";
+	//				DrawText(SelectionString, FColor::White, ScreenCoords.X - 25.0f, ScreenCoords.Y + 25.0f, nullptr, 1.5f);
+	//			}
+	//		}
+	//		
+	//	}
+	//}
 
 }
